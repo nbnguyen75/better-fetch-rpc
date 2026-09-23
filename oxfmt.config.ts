5e9af7f1c834996647c36228ba21873da748027b
nbnguyen75 <wynn.nguyen.dev@gmail.com>
2026-09-23 14:49:40 +0700

    init project

---
+++ oxfmt.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'oxfmt';
+
+export default defineConfig({
+	useTabs: true,
+	singleQuote: true,
+	trailingComma: 'none',
+	printWidth: 100,
+	sortPackageJson: false
+});
